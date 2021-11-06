c36bb027803a816743966ea7e9eac97205cb22c7
tsl666 <tsl3627@gmail.com>
2021-11-06 23:50:27 +0800

    ..

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,6 @@
 export const baseUrl = 'https://api.wetrue.io'
 export const nodeUrl = 'https://node.aechina.io/'
 export const aeknow = 'https://www.aeknow.org/'
-export const version = '2.4.9'
+export const version = '2.5.0'
 export const source = 'WeTrue App'
 export const wttContract ='ct_uGk1rkSdccPKXLzS259vdrJGTWAY9sfgVYspv6QYomxvWZWBM'
