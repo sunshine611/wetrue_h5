beebdffe022935b8077f9d27e685e99c18e410b7
Lfh2msn <liufanghong@msn.com>
2021-12-20 22:26:32 +0800

    第三方设置语言

--- src/util/thirdPartySource/source.js
+++ src/util/thirdPartySource/source.js
@@ -9,9 +9,9 @@ export const setThirdPartySource = function(option){
         store.commit("user/SET_PASSWORD", option.source); //伪装存在密码
         setConfigInfo(option.userAddress); //设置 configInfo
         if (option.language === "en") { //设置语言
-            setStore("language", "en");
+             store.commit("user/SET_LANGUAGE", "en");
         } else {
-            setStore("language", "zh-cn");
+            store.commit("user/SET_LANGUAGE", "zh-cn");
         }
     }
 }
