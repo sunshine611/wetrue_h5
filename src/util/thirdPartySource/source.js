30636ab507f6582419cfe699d4a8cc81fea59e24
Lfh2msn <liufanghong@msn.com>
2021-12-21 20:10:38 +0800

    Update source.js

--- src/util/thirdPartySource/source.js
+++ src/util/thirdPartySource/source.js
@@ -4,14 +4,14 @@ import Backend from "@/util/backend";
 export const setThirdPartySource = (option) =>{
     //设置第三方来源
 	if (option.source === "box" && !!option.userAddress) {
+        if (option.language === "en") { //设置语言
+            store.commit("user/SET_LANGUAGE", "en");
+       } else {
+            store.commit("user/SET_LANGUAGE", "zh-cn");
+       }
         store.commit("user/SET_TOKEN", option.userAddress); //设置token
         store.commit("user/SET_TPSOURCE", option.source); //标记第三方来源
         store.commit("user/SET_PASSWORD", option.source); //伪装存在密码
         Backend.setConfigInfo(option.userAddress); //设置 configInfo
-        if (option.language === "en") { //设置语言
-             store.commit("user/SET_LANGUAGE", "en");
-        } else {
-            store.commit("user/SET_LANGUAGE", "zh-cn");
-        }
     }
 }
