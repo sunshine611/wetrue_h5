fec7da6b92796eaee2c43c3c8b966831b0d1a96c
Lfh2msn <liufanghong@msn.com>
2021-12-26 11:42:34 +0800

    合并版本257

--- src/config/config.js
+++ src/config/config.js
@@ -4,6 +4,6 @@ export const aeknow     = 'https://www.aeknow.org'
 export const aeMdw      = 'https://mainnet.aeternity.io/mdw'
 export const aeExplorer = 'https://explorer.aeternity.io'
 
-export const version = '2.5.6'
+export const version = '2.5.7'
 export const source = 'WeTrue'
 export const wttContract ='ct_uGk1rkSdccPKXLzS259vdrJGTWAY9sfgVYspv6QYomxvWZWBM'
