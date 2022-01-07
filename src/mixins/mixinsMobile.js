1f67e79c43bd4b19517e432c2ce169a40538096a
Lfh2msn <liufanghong@msn.com>
2022-01-07 23:10:10 +0800

    Update mixinsMobile.js

--- src/mixins/mixinsMobile.js
+++ src/mixins/mixinsMobile.js
@@ -218,20 +218,18 @@ const mixins = {
         },
         //话题及@高亮
         topicHighlight(value) {
-            let expt, expm, exps;
-            expt = /#([x80-xff\u4e00-\u9fa5\w ,，.。!！-？·\?æÆ](?!<br>#)(?!\[ST\])){1,25}#/g;
-            expm = /@[\p{L}\d]+.chain/gu;
-            exps = /#([^#]*)(\[ST\])#/g;
+            let expt = /#([x80-xff\u4e00-\u9fa5\w ,，.。!！-？·\?æÆ](?!<br>#)(?!\[ST\])){1,25}#/g;
             value = value.replace(expt, (item) => {
                 let newVal = `<text class="topic-text">${item}</text>`;
                 return newVal;
             });
+            let expm = /@[\p{L}\d]+.chain/gu;
             value = value.replace(expm, (item) => {
                 let newVal = `<text class="mentions-text">${item}</text>`;
                 return newVal;
             });
-            value = value.replace(exps, `<text class="topic-text">⚡$1</text>`);
-        
+            //let exps = /#([^#]*)(\[ST\])#/g;
+            //value = value.replace(exps, `<text class="topic-text">⚡$1</text>`);
             return value;
         },
         //复制粘贴板
