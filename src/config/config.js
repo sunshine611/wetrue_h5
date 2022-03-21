1a1a3e363d76bd531dcf59d03b3ae97afed792c3
Lfh2msn <liufanghong@msn.com>
2022-03-21 22:13:47 +0800

    修改aeknow api

--- src/config/config.js
+++ src/config/config.js
@@ -11,9 +11,10 @@ export const aeMdw       = 'https://testnet.aeternity.io/mdw'
 export const aeExplorer  = 'https://explorer.testnet.aeternity.io'
 export const chatapi     = 'http://192.168.1.111:50001'
 */
-export const version     = '2.5.9'
+export const version     = '2.7.0'
 export const source      = 'WeTrue'
-export const aeknow      = 'https://www.aeknow.org'
+//export const aeknow      = 'https://www.aeknow.org'
+export const aeknow      = 'https://api.wetrue.io/Aeknow'
 export const compilerUrl = 'https://compiler.aeternity.io'
 export const wttContract ='ct_uGk1rkSdccPKXLzS259vdrJGTWAY9sfgVYspv6QYomxvWZWBM'
 export const chatRommKeyIv = {key: '20200425FEAACD66',iv: '66DCAAEF20200425'}
